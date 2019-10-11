ee356bd4b27dc9e8060b99dd010e9def5f4cd8e5
Matthew Nelson <05nelsonm@protonmail.com>
2019-10-11 19:54:06 -0400

    bug fixes and removed root user ssh login

--- control_dojo.sh
+++ control_dojo.sh
@@ -17,7 +17,9 @@ PATH_T0_DOJO_DOT_SH="/home/path/to/dojo_dir/docker/my-dojo/"
 # change `EXTERNAL_BITCOIND` to "yes", and `BITCOIN_DIR` to the path for where your .bitcoin
 # directory is located (most likely in /home/$USER/.bitcoin).
 
-EXTERNAL_BITCOIND="no"
+EXTERNAL_BITCOIND="yes"
+
+# And change your `/home/username/.bitcoin` directory
 
 #    ***** example entry: "/home/matthew/.bitcoin/" *****
 
@@ -27,26 +29,32 @@ BITCOIN_DIR="/home/path/to/.bitcoin/"
 ######### Options for using this sript on a machine Dojo isn't installed on ###################
 
 # If you want to use this script remotely from a different machine, change SSH_OPTION to "yes"
-# and make sure you have the remote machine's rsa pubkeys in your Dojo machine`s root
-# user`s `/root/.ssh/authorized_keys` file. See the README.md `ssh` section for more info.
+# and modify the fields below appropriately.
+
+# Also make sure you have the remote machine's public ssh key in your Dojo machine user`s
+# `~/your_user_name/.ssh/authorized_keys` file.
+
+# See the README.md `ssh` section for more info on how to set that up for passwordless ssh login
 
 SSH_OPTION="no"
 SSH_PORT="22"
+USER_NAME="your_user_name"
 IP="xxx.xxx.x.xxx"
 
 ###############################################################################################
 ###############################################################################################
 
-SSH_CMD="ssh -tt -p $SSH_PORT root@$IP"
+SSH_CMD="ssh -tt -p $SSH_PORT $USER_NAME@$IP"
 CMD1="cd $PATH_T0_DOJO_DOT_SH"
+CMD2="cd $BITCOIN_DIR"
 
 if [ "$EXTERNAL_BITCOIND" = "no" ]; then
-        DOJO_CMD=("---EXIT---" "help" "version" "bitcoin-cli" "logs" "onion" "restart" \
-                        "start" "stop" "upgrade" "clean" "install" "uninstall")
+	DOJO_CMD=("---EXIT---" "help" "version" "bitcoin-cli" "logs" "onion" "restart" \
+			"start" "stop" "upgrade" "clean" "install" "uninstall")
 else
-        DOJO_CMD=("---EXIT---" "help" "version" "START bitcoind" "STOP bitcoind" \
-                        "bitcoin-cli" "logs" "onion" "restart" "start" "stop" "upgrade" \
-                        "clean" "install" "uninstall")
+	DOJO_CMD=("---EXIT---" "help" "version" "START bitcoind" "STOP bitcoind" \
+			"bitcoin-cli" "logs" "onion" "restart" "start" "stop" "upgrade" \
+			"clean" "install" "uninstall")
 fi
 
 LOG_MODULES=("---BACK---" "bitcoind" "db" "tor" "api" "tracker" "pushtx" "pushtx-orchest")
@@ -59,72 +67,80 @@ while true; do
         GOBACK="no"
         echo "------------------------------------------------------------------------"
         echo "------------------- SAMOURAI DOJO INTERACTION SCRIPT -------------------"
-        echo "------------------------------------------------------------------------"
+	echo "------------------------------------------------------------------------"
 
-        # Display options for user selection
+	# Display options for user selection
         for ((i=0; i < $LENGTH_DOJO_CMD; i++)); do
 
-                # Prints first element in column 1 by itself
-                if [ $i -eq 0 ]; then
-                        echo " "
-                        echo "                  $i) ${DOJO_CMD[$i]}"
+		# Prints first element in column 1 by itself
+		if [ $i -eq 0 ]; then
+			echo " "
+			echo "                  $i) ${DOJO_CMD[$i]}"
 
-                # If number is odd print to column 1 and allow for next element to be printed
-                # in column 2
-                elif [ $((i%2)) -ne 0 ]; then
+		# If number is odd print to column 1 and allow for next element to be printed
+		# in column 2
+		elif [ $((i%2)) -ne 0 ]; then
 
-                        # If it`s the last entry, ensure it`s not using `echo -n`
-                        if [[ $i -eq $LENGTH_DOJO_CMD-1 ]]; then
-                                echo " "
-                                echo "                  $i) ${DOJO_CMD[$i]}"
-                        else
-                                echo " "
-                                echo -n "                  $i) ${DOJO_CMD[$i]}"
+			# If it`s the last entry, ensure it`s not using `echo -n`
+			if [[ $i -eq $LENGTH_DOJO_CMD-1 ]]; then
+				echo " "
+				echo "                  $i) ${DOJO_CMD[$i]}"
+			else
+		                echo " "
+		                echo -n "                  $i) ${DOJO_CMD[$i]}"
 
-                                # Setup spacing for column 2
-                                temp22="${DOJO_CMD[$i]}"
-                                for ((j=0; j < 25 - $( expr length "$i) $temp22" ); j++)); do
-                                        echo -n " "
-                                done
-                                unset temp22
-                        fi
-                else
-                        echo "$i) ${DOJO_CMD[$i]}"
-                fi
+				# Setup spacing for column 2
+				temp22="${DOJO_CMD[$i]}"
+				for ((j=0; j < 25 - $( expr length "$i) $temp22" ); j++)); do
+					echo -n " "
+				done
+				unset temp22
+			fi
+		else
+			echo "$i) ${DOJO_CMD[$i]}"
+		fi
         done
 
-        # Prompt for user selection
+	# Prompt for user selection
         while true; do
                 echo " "
                 read -p "Please enter a number corresponding to what you'd like to do: " NUM
                 echo "------------------------------------------------------------------------"
 
-                # Numbers outside available options loop back
+		# Numbers outside available options loop back
                 if [[ $NUM -lt 0 || $NUM -gt $LENGTH_DOJO_CMD-1 ]]; then
-                        unset NUM
+			unset NUM
                         echo " "
                         echo "Option not available, please try again..."
                         echo " "
                         sleep 1
-                        GOBACK="yes"
-                        break
+			GOBACK="yes"
+			break
 
-                # Exit script
+		# Exit script
                 elif [ $NUM -eq 0 ]; then
                         exit 0
 
-                # For users to start up their own instance of bitcoind
-                elif [ "${DOJO_CMD[$NUM]}" = "START bitcoind" ]; then
-                        bitcoind --daemon
-                        echo "bitcoind has been started..."
-                        echo " "
-                # For users to stop their own instance of bitcoind
-                elif [ "${DOJO_CMD[$NUM]}" = "STOP bitcoind" ]; then
-                        bitcoin-cli stop
+		# For users to start up their own instance of bitcoind
+		elif [ "${DOJO_CMD[$NUM]}" = "START bitcoind" ]; then
+			if [ "$SSH_OPTION" = "yes" ]; then
+				$SSH_CMD "bitcoind --daemon"
+                        else
+				bitcoind --daemon
+			fi
+			echo "bitcoind has been started..."
+			echo " "
+		# For users to stop their own instance of bitcoind
+		elif [ "${DOJO_CMD[$NUM]}" = "STOP bitcoind" ]; then
+			if [ "$SSH_OPTION" = "yes" ]; then
+				$SSH_CMD "bitcoin-cli stop"
+                        else
+	                        bitcoin-cli stop
+			fi
                         echo "bitcoind has been stopped..."
                         echo " "
 
-                # Prompt for confirmation on "restart" "start" "stop" "upgrade" "clean" "install" "uninstall" options
+		# Prompt for confirmation on "restart" "start" "stop" "upgrade" "clean" "install" "uninstall" options
                 elif [[ $NUM -gt $LENGTH_DOJO_CMD-7 ]]; then
                         while true; do
                                 echo " "
@@ -139,10 +155,10 @@ while true; do
                         break
                 fi
 
-                # Prompt again if user selects uninstall to mitigate accidental wiping of data
-                if [[ "${DOJO_CMD[$NUM]}" = "uninstall" && "$CONFIRM" = "yes" ]]; then
-                        unset CONFIRM
-                        while true; do
+		# Prompt again if user selects uninstall to mitigate accidental wiping of data
+		if [[ "${DOJO_CMD[$NUM]}" = "uninstall" && "$CONFIRM" = "yes" ]]; then
+			unset CONFIRM
+			while true; do
                                 echo " "
                                 read -p "Are you ABSOLUTELY sure you'd like to ${DOJO_CMD[$NUM]}? [y/n]: " yn
                                 case $yn in
@@ -151,15 +167,15 @@ while true; do
                                         * ) echo "Please answer y or n."
                                 esac
                         done
-                fi
+		fi
 
-                # Break primary while loop if user selects yes for confirmations
+		# Break primary while loop if user selects yes for confirmations
                 if [[ "$GOBACK" = "yes" || "$CONFIRM" = "yes" ]]; then
                         break
                 fi
         done
 
-        # Options for logs
+	# Options for logs
         if [ "${DOJO_CMD[$NUM]}" = "logs" ]; then
 
                 while true; do
@@ -169,55 +185,55 @@ while true; do
                         echo "---------------------------- Available Logs ----------------------------"
                         echo " "
 
-                        # Display options for user selection
-                        for ((i=0; i < $LENGTH_LOG_MOD; i++)); do
+			# Display options for user selection
+		        for ((i=0; i < $LENGTH_LOG_MOD; i++)); do
 
-                                # Prints first element in column 1 by itself
-                                if [ $i -eq 0 ]; then
-                                        echo " "
-                                        echo "                  $i) ${LOG_MODULES[$i]}"
+		                # Prints first element in column 1 by itself
+		                if [ $i -eq 0 ]; then
+		                        echo " "
+		                        echo "                  $i) ${LOG_MODULES[$i]}"
 
-                                # If number is odd print to column 1 and allow for next element to be printed in column 2
-                                elif [ $((i%2)) -ne 0 ]; then
+		                # If number is odd print to column 1 and allow for next element to be printed in column 2
+		                elif [ $((i%2)) -ne 0 ]; then
 
-                                        # If it`s the last entry, ensure it`s not using `echo -n`
-                                        if [[ $i -eq $LENGTH_LOG_MOD-1 ]]; then
-                                                echo " "
-                                                echo "                  $i) ${LOG_MODULES[$i]}"
-                                        else
-                                                echo " "
-                                                echo -n "                  $i) ${LOG_MODULES[$i]}"
+					# If it`s the last entry, ensure it`s not using `echo -n`
+					if [[ $i -eq $LENGTH_LOG_MOD-1 ]]; then
+						echo " "
+						echo "                  $i) ${LOG_MODULES[$i]}"
+					else
+			                        echo " "
+			                        echo -n "                  $i) ${LOG_MODULES[$i]}"
 
-                                                # Setup spacing for column 2
-                                                temp33="${LOG_MODULES[$i]}"
-                                                for ((j=0; j < 25 - $( expr length "$i) $temp33" ); j++)); do
-                                                        echo -n " "
-                                                done
-                                                unset temp33
-                                        fi
-                                else
-                                        echo "$i) ${LOG_MODULES[$i]}"
-                                fi
-                        done
+			                        # Setup spacing for column 2
+			                        temp33="${LOG_MODULES[$i]}"
+			                        for ((j=0; j < 25 - $( expr length "$i) $temp33" ); j++)); do
+			                                echo -n " "
+			                        done
+						unset temp33
+					fi
+		                else
+		                        echo "$i) ${LOG_MODULES[$i]}"
+		                fi
+		        done
 
-                        echo " "
+			echo " "
                         echo "-------------- press CTRL+C to exit the log when finished --------------"
                         echo " "
                         read -p "Please enter a number corresponding to what logs you'd like to view: " LNUM
                         echo " "
 
-                        # Numbers outside available options loop back
+			# Numbers outside available options loop back
                         if [[ $LNUM -lt 0 || $LNUM -gt $LENGTH_LOG_MOD-1 ]]; then
                                 echo "Option not available, please try again..."
                                 echo " "
-                                sleep 2
+				sleep 2
 
-                        # User selection to go back
+			# User selection to go back
                         elif [ $LNUM -eq 0 ]; then
                                 GOBACK="yes"
                                 break
 
-                        # Additional command options for api tracker pushtx pushtx-orchest
+			# Additional command options for api tracker pushtx pushtx-orchest
                         elif [ $LNUM -gt 3 ]; then
                                 echo "Available options to enter are '-d [VALUE]' **OR** '-n [VALUE]'"
                                 echo " "
@@ -229,28 +245,28 @@ while true; do
                         fi
 
                         if [ "$EXECUTE" = "yes" ]; then
-                                unset EXECUTE
+				unset EXECUTE
                                 trap "echo" SIGINT SIGTERM
-                                if [ "SSH_OPTION" = "yes" ]; then
-                                        if [[ "$EXTERNAL_BITCOIND" = "yes" && "${LOG_MODULES[$LNUM]}" = "bitcoind" ]]; then
-                                                $SSH_CMD "tail -f $BITCOIN_DIR/debug.log"
-                                        else
-                                                $SSH_CMD "$CMD1 && ./dojo.sh logs ${LOG_MODULES[$LNUM]} $AVAIL_OPTIONS"
-                                        fi
-                                else
-                                        if [[ "$EXTERNAL_BITCOIND" = "yes" && "${LOG_MODULES[$LNUM]}" = "bitcoind" ]]; then
-                                                tail -f $BITCOIN_DIR/debug.log
-                                        else
-                                                $CMD1 && sudo ./dojo.sh logs ${LOG_MODULES[$LNUM]} $AVAIL_OPTIONS
-                                        fi
-                                fi
+				if [ "$SSH_OPTION" = "yes" ]; then
+					if [[ "$EXTERNAL_BITCOIND" = "yes" && "${LOG_MODULES[$LNUM]}" = "bitcoind" ]]; then
+						$SSH_CMD "$CMD2 && tail -f debug.log"
+					else
+						$SSH_CMD "$CMD1 && sudo ./dojo.sh logs ${LOG_MODULES[$LNUM]} $AVAIL_OPTIONS"
+					fi
+				else
+					if [[ "$EXTERNAL_BITCOIND" = "yes" && "${LOG_MODULES[$LNUM]}" = "bitcoind" ]]; then
+						$CMD2 && tail -f debug.log
+					else
+						$CMD1 && sudo ./dojo.sh logs ${LOG_MODULES[$LNUM]} $AVAIL_OPTIONS
+					fi
+				fi
                                 trap - SIGINT SIGTERM
                         fi
 
                 done
         fi
 
-        # Bitcoin-cli interaction
+	# Bitcoin-cli interaction
         if [ "${DOJO_CMD[$NUM]}" = "bitcoin-cli" ]; then
 
                 while true; do
@@ -260,19 +276,19 @@ while true; do
                         if [[ "$BITCOIN_CLI_CMD" = "X" || "$BITCOIN_CLI_CMD" = "x" ]]; then
                                 break
                         else
-                                if [ "SSH_OPTION" = "yes" ]; then
-                                        if [ "$EXTERNAL_BITCOIND" = "no" ]; then
-                                                $SSH_CMD "$CMD1 && ./dojo.sh ${DOJO_CMD[$NUM]} $BITCOIN_CLI_CMD"
-                                        else
-                                                $SSH_CMD "bitcoin-cli $BITCOIN_CLI_CMD"
-                                        fi
-                                else
-                                        if [ "$EXTERNAL_BITCOIND" = "no" ]; then
-                                                $CMD1 && sudo ./dojo.sh ${DOJO_CMD[$NUM]} $BITCOIN_CLI_CMD
+				if [ "$SSH_OPTION" = "yes" ]; then
+					if [ "$EXTERNAL_BITCOIND" = "no" ]; then
+						$SSH_CMD "$CMD1 && sudo ./dojo.sh ${DOJO_CMD[$NUM]} $BITCOIN_CLI_CMD"
+					else
+						$SSH_CMD "bitcoin-cli $BITCOIN_CLI_CMD"
+					fi
+				else
+					if [ "$EXTERNAL_BITCOIND" = "no" ]; then
+						$CMD1 && sudo ./dojo.sh ${DOJO_CMD[$NUM]} $BITCOIN_CLI_CMD
                                         else
                                                 bitcoin-cli $BITCOIN_CLI_CMD
                                         fi
-                                fi
+				fi
                                 echo "------------------------------------------------------------------------"
                         fi
 
@@ -280,11 +296,11 @@ while true; do
 
         elif [ "$GOBACK" != "yes" ]; then
 
-                if [ "SSH_OPTION" = "yes" ]; then
-                        $SSH_CMD "$CMD1 && ./dojo.sh ${DOJO_CMD[$NUM]}"
-                else
-                        $CMD1 && sudo ./dojo.sh ${DOJO_CMD[$NUM]}
-                fi
+		if [ "$SSH_OPTION" = "yes" ]; then
+			$SSH_CMD "$CMD1 && sudo ./dojo.sh ${DOJO_CMD[$NUM]}"
+		else
+			$CMD1 && sudo ./dojo.sh ${DOJO_CMD[$NUM]}
+		fi
 
                 echo " "
                 while true; do
